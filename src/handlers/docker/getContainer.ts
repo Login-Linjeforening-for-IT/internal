815364fddfab5814ff79e8e63ed108774b3eb984
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-29 20:13:53 +0100

    Added details

--- src/handlers/docker/getContainer.ts
+++ src/handlers/docker/getContainer.ts
@@ -12,8 +12,12 @@ export default async function getDockerContainer(req: FastifyRequest, res: Fasti
             `docker ps -a --format "{{.ID}}|{{.Names}}|{{.Status}}|{{.RunningFor}}"`
         )
 
-        const lines = stdout.split('\n').filter(Boolean)
+        const { stdout: inspectOut } = await execAsync(`docker inspect ${id}`)
+        const details = JSON.parse(inspectOut)[0]
 
+        const { stdout: logsOut } = await execAsync(`docker logs ${id}`)
+        const logs = logsOut.split('\n').filter(Boolean)
+        const lines = stdout.split('\n').filter(Boolean)
         const containers = lines.map(line => {
             const [cid, name, status, uptime] = line.split('|')
             return { id: cid, name, status, uptime }
@@ -32,7 +36,7 @@ export default async function getDockerContainer(req: FastifyRequest, res: Fasti
 
         return res.send({
             service,
-            container,
+            container: { ...container, details, logs },
             related: relatedContainers
         })
 
